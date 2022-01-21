b5aaee15a552b66e4652a0dda1efe0c61a7095e0
Adriel <adriel.fariasan@gmail.com>
2022-01-21 16:16:58 -0300

    Adiciona arquivos iniciais

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
