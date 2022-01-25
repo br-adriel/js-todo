4aa02d7e8eb9536db4dc7713ccb3173f2874d53d
Adriel <adriel.fariasan@gmail.com>
2022-01-24 23:09:21 -0300

    Adiciona loaders para css

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports = {
     index: "./src/index.js",
   },
   devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
