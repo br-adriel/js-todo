f919e03fe9b2c2d7389f5e94723913f55211b5ad
Adriel <adriel.fariasan@gmail.com>
2022-01-25 21:54:37 -0300

    Encapsula form de usuario dentro de uma funcao

--- src/componentes/formUsuario.js
+++ src/componentes/formUsuario.js
@@ -1,10 +1,13 @@
 import { gerCampoForm } from "./geradoresHtml";
 
-const campoUsuario = gerCampoForm("Usuário:", "text", "formUsuario");
-const campoSenha = gerCampoForm("Senha:", "password", "formSenha");
+function formUsuario() {
+  const campoUsuario = gerCampoForm("Usuário:", "text", "formUsuario");
+  const campoSenha = gerCampoForm("Senha:", "password", "formSenha");
 
-const form = document.createElement("form");
-form.appendChild(campoUsuario);
-form.appendChild(campoSenha);
+  const form = document.createElement("form");
+  form.appendChild(campoUsuario);
+  form.appendChild(campoSenha);
+  return form;
+}
 
-export default form;
+export default formUsuario;
