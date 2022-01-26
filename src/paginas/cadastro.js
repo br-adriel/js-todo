858b45ed007ce5a60463141b11f734bc995e6010
Adriel <adriel.fariasan@gmail.com>
2022-01-25 21:55:37 -0300

    Encapsula pagina e implementa link para login

--- src/paginas/cadastro.js
+++ src/paginas/cadastro.js
@@ -1,47 +1,61 @@
 import formCadastro from "./../componentes/formUsuario";
 import { gerBotao, gerCampoForm } from "./../componentes/geradoresHtml";
 import "./cadastro.css";
+import pagLogin from "./login";
 
-// titulo do card
-const titulo = document.createElement("h2");
-titulo.innerText = "Criar conta";
+const pagCadastro = (() => {
+  // titulo do card
+  const titulo = document.createElement("h2");
+  titulo.innerText = "Criar conta";
 
-// campo de confirmacao de senha
-let textoAjuda = "Os dados ficam salvo apenas localmente no seu dispositivo, ";
-textoAjuda +=
-  "mesmo assim recomendados que utilize uma senha que não use em outras contas";
+  // campo de confirmacao de senha
+  let textoAjuda =
+    "Os dados ficam salvo apenas localmente no seu dispositivo, ";
+  textoAjuda +=
+    "mesmo assim recomendados que utilize uma senha que não use em outras contas";
 
-const inputSenha2 = gerCampoForm(
-  "Confirmação de senha",
-  "password",
-  "formSenha2",
-  textoAjuda
-);
-formCadastro.appendChild(inputSenha2);
+  const inputSenha2 = gerCampoForm(
+    "Confirmação de senha",
+    "password",
+    "formSenha2",
+    textoAjuda
+  );
 
-// link caso o usuario tenha conta
-const linkLogin = document.createElement("a");
-linkLogin.setAttribute("href", "#");
-linkLogin.innerText = "Faça login";
+  const form = formCadastro();
+  form.appendChild(inputSenha2);
 
-const pLogin = document.createElement("p");
-pLogin.innerText = "Já possui conta? ";
-pLogin.appendChild(linkLogin);
-formCadastro.appendChild(pLogin);
+  // link caso o usuario tenha conta
+  const linkLogin = document.createElement("a");
+  linkLogin.setAttribute("href", "#");
+  linkLogin.innerText = "Faça login";
+  linkLogin.addEventListener("click", () => {
+    const main = document.getElementById("content");
+    main.appendChild(pagLogin);
+    main.firstChild.style.display = "none";
+    main.removeChild(main.firstChild);
+    main.firstChild.style.display = "flex";
+  });
 
-// botao para envio do form
-const btnCadastrar = gerBotao("submit", "Cadastrar-se");
-formCadastro.appendChild(btnCadastrar);
+  const pLogin = document.createElement("p");
+  pLogin.innerText = "Já possui conta? ";
+  pLogin.appendChild(linkLogin);
+  form.appendChild(pLogin);
 
-// card para os elementos
-const cardCadastro = document.createElement("div");
-cardCadastro.classList.add("card");
-cardCadastro.appendChild(titulo);
-cardCadastro.appendChild(formCadastro);
+  // botao para envio do form
+  const btnCadastrar = gerBotao("submit", "Cadastrar-se");
+  form.appendChild(btnCadastrar);
 
-// pagina de cadastro
-const paginaCadastro = document.createElement("section");
-paginaCadastro.setAttribute("id", "paginaCadastro");
-paginaCadastro.appendChild(cardCadastro);
+  // card para os elementos
+  const cardCadastro = document.createElement("div");
+  cardCadastro.classList.add("card");
+  cardCadastro.appendChild(titulo);
+  cardCadastro.appendChild(form);
 
-export default paginaCadastro;
+  // pagina de cadastro
+  const paginaCadastro = document.createElement("section");
+  paginaCadastro.setAttribute("id", "paginaCadastro");
+  paginaCadastro.appendChild(cardCadastro);
+  return paginaCadastro;
+})();
+
+export default pagCadastro;
