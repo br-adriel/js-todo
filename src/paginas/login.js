36e6350afe45afbb6e09d9220b372c8b0469c9bd
Adriel <adriel.fariasan@gmail.com>
2022-01-25 21:55:51 -0300

    Encapsula pagina e implementa link para cadastro

--- src/paginas/login.js
+++ src/paginas/login.js
@@ -1,34 +1,48 @@
 import formLogin from "./../componentes/formUsuario";
 import { gerBotao } from "./../componentes/geradoresHtml";
+import pagCadastro from "./cadastro";
 import "./login.css";
 
-// titulo do card
-const titulo = document.createElement("h2");
-titulo.innerText = "Entrar";
+const pagLogin = (() => {
+  // titulo do card
+  const titulo = document.createElement("h2");
+  titulo.innerText = "Entrar";
 
-// link caso o usuario nao tenha conta
-const linkCriarConta = document.createElement("a");
-linkCriarConta.setAttribute("href", "#");
-linkCriarConta.innerText = "Cadastre-se";
+  // link caso o usuario nao tenha conta
+  const linkCriarConta = document.createElement("a");
+  linkCriarConta.setAttribute("href", "#");
+  linkCriarConta.innerText = "Cadastre-se";
+  linkCriarConta.addEventListener("click", (e) => {
+    const main = document.getElementById("content");
+    main.appendChild(pagCadastro);
+    main.firstChild.style.display = "none";
+    main.removeChild(main.firstChild);
+    main.firstChild.style.display = "flex";
+  });
 
-const pCriarConta = document.createElement("p");
-pCriarConta.innerText = "Não possui conta? ";
-pCriarConta.appendChild(linkCriarConta);
-formLogin.appendChild(pCriarConta);
+  const pCriarConta = document.createElement("p");
+  pCriarConta.innerText = "Não possui conta? ";
+  pCriarConta.appendChild(linkCriarConta);
 
-// botao para envio do form
-const btnEntrar = gerBotao("submit", "Entrar");
-formLogin.appendChild(btnEntrar);
+  const form = formLogin();
+  form.appendChild(pCriarConta);
 
-// card para os elementos
-const cardLogin = document.createElement("div");
-cardLogin.classList.add("card");
-cardLogin.appendChild(titulo);
-cardLogin.appendChild(formLogin);
+  // botao para envio do form
+  const btnEntrar = gerBotao("submit", "Entrar");
+  form.appendChild(btnEntrar);
 
-// pagina de login
-const paginaLogin = document.createElement("section");
-paginaLogin.setAttribute("id", "paginaLogin");
-paginaLogin.appendChild(cardLogin);
+  // card para os elementos
+  const cardLogin = document.createElement("div");
+  cardLogin.classList.add("card");
+  cardLogin.appendChild(titulo);
+  cardLogin.appendChild(form);
 
-export default paginaLogin;
+  // pagina de login
+  const paginaLogin = document.createElement("section");
+  paginaLogin.setAttribute("id", "paginaLogin");
+  paginaLogin.appendChild(cardLogin);
+
+  return paginaLogin;
+})();
+
+export default pagLogin;
