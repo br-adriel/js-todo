40aa27a3e8a10be2ab5544edc07291d4305ee354
Adriel <adriel.fariasan@gmail.com>
2022-01-27 15:54:31 -0300

    Adiciona campo para as mensagens serem exibidas

--- src/componentes/formUsuario.js
+++ src/componentes/formUsuario.js
@@ -1,10 +1,14 @@
 import { gerCampoForm } from "./geradoresHtml";
 
 function formUsuario() {
+  const campoMsg = document.createElement("div");
+  campoMsg.setAttribute("id", "campoMsgForm");
+
   const campoUsuario = gerCampoForm("Usuário:", "text", "formUsuario");
   const campoSenha = gerCampoForm("Senha:", "password", "formSenha");
 
   const form = document.createElement("form");
+  form.appendChild(campoMsg);
   form.appendChild(campoUsuario);
   form.appendChild(campoSenha);
   return form;
