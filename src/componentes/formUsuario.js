e29b4c2fe42ed4c544d9628b727474b0e65ba756
Adriel <adriel.fariasan@gmail.com>
2022-01-27 16:10:52 -0300

    Adiciona minlength a campos do formUsuario

--- src/componentes/formUsuario.js
+++ src/componentes/formUsuario.js
@@ -4,8 +4,14 @@ function formUsuario() {
   const campoMsg = document.createElement("div");
   campoMsg.setAttribute("id", "campoMsgForm");
 
-  const campoUsuario = gerCampoForm("Usuário:", "text", "formUsuario");
-  const campoSenha = gerCampoForm("Senha:", "password", "formSenha");
+  const campoUsuario = gerCampoForm("Usuário:", "text", "formUsuario", "", [
+    ["minlength", "3"],
+    ["required", "true"],
+  ]);
+  const campoSenha = gerCampoForm("Senha:", "password", "formSenha", "", [
+    ["minlength", "8"],
+    ["required", "true"],
+  ]);
 
   const form = document.createElement("form");
   form.appendChild(campoMsg);
