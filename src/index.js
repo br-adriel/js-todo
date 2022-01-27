b97594157ffda53d17ca06b41250d5c84d94876c
Adriel <adriel.fariasan@gmail.com>
2022-01-27 16:59:20 -0300

    Implementa login e criacao de usuario padrao

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import Tarefa from "./classes/Tarefa";
 const usuarios = [];
 const usuarioAtivo = [];
 
+// Página de cadastro
 const pagCadastro = (() => {
   // titulo do card
   const titulo = document.createElement("h2");
@@ -56,10 +57,27 @@ const pagCadastro = (() => {
       const msg = gerMensagem("As senhas não correspondem", "aviso");
       campoMsg.appendChild(msg);
     } else {
+      // cria data referente a dia atual mais um dia
+      const data = new Date();
+      data.setTime(data.getTime() + 8640000);
+
+      // Cria uma tarefa padrão para novo usuário
+      const novaTarefa = new Tarefa("Tarefa 1", "Minha primeira tarefa", data);
+
+      // Cria uma lista padrão para o novo usuário
+      const novaLista = new Lista("Minhas tarefas");
+      novaLista.tarefas.push(novaTarefa);
+
+      // Cria um novo usuario com lista e tarefa padrao
       const novoUsuario = new Usuario(usuarioForm, senhaForm);
+      novoUsuario.listas.push(novaLista);
+
+      // Adiciona novo usuario a lista de usuarios e define como o ativo
       usuarios.push(novoUsuario);
       usuarioAtivo.push(novoUsuario);
-      console.log("Conta criada e usuário logado");
+
+      // Carrega página inicial
+      gerVisualizacao(pagInicial);
     }
   }
 
@@ -93,6 +111,7 @@ const pagCadastro = (() => {
   return paginaCadastro;
 })();
 
+// Página de Login
 const pagLogin = (() => {
   // titulo do card
   const titulo = document.createElement("h2");
@@ -110,13 +129,33 @@ const pagLogin = (() => {
   pCriarConta.innerText = "Não possui conta? ";
   pCriarConta.appendChild(linkCriarConta);
 
-  const form = formUsuario();
-  form.setAttribute("id", "formUsuario");
-  form.appendChild(pCriarConta);
-
   // botao para envio do form
   const btnEntrar = gerBotao("submit", "Entrar");
+
+  // formulario de login
+  const form = formUsuario();
+  form.appendChild(pCriarConta);
   form.appendChild(btnEntrar);
+  form.addEventListener("submit", (e) => enviarForm(e));
+
+  // lida com o envio do formulario de login
+  function enviarForm(e) {
+    e.preventDefault();
+
+    const usuarioForm = document.getElementById("formUsuario").value;
+    const senhaForm = document.getElementById("formSenha").value;
+
+    if (
+      usuarios.some((u) => u.usuario === usuarioForm && u.senha === senhaForm)
+    ) {
+      usuarios.push(usuarios.filter((u) => u.usuario === usuarioForm));
+      gerVisualizacao(pagInicial);
+    } else {
+      const campoMsg = document.getElementById("campoMsgForm");
+      const msg = gerMensagem("Credenciais inválidas", "erro");
+      campoMsg.appendChild(msg);
+    }
+  }
 
   // card para os elementos
   const cardLogin = document.createElement("div");
@@ -132,5 +171,16 @@ const pagLogin = (() => {
   return paginaLogin;
 })();
 
+// Página Inicial
+const pagInicial = (() => {
+  const paginaInicial = document.createElement("section");
+  paginaInicial.setAttribute("id", "paginaInicial");
+  return paginaInicial;
+})();
+
 const conteudo = document.getElementById("content");
-conteudo.appendChild(pagLogin);
+if (usuarioAtivo.length == 0) {
+  conteudo.appendChild(pagLogin);
+} else {
+  conteudo.appendChild(pagInicial);
+}
