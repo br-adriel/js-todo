8ac5d2f59b09f1ebffc88c7f91b1d085fcd0b734
Adriel <adriel.fariasan@gmail.com>
2022-02-06 21:08:38 -0300

    Atualiza classe para implementacao de localStorage

--- src/classes/Usuario.js
+++ src/classes/Usuario.js
@@ -1,22 +1,8 @@
 class Usuario {
-  #senha;
   constructor(usuario, senha) {
     this.usuario = usuario;
     this.listas = [];
-    this.#senha = senha;
-  }
-
-  mudarSenha(senhaAtual, novaSenha) {
-    if (senhaAtual === this.#senha) {
-      this.#senha = novaSenha;
-    }
-  }
-
-  login(senha) {
-    if (senha === this.#senha) {
-      return true;
-    }
-    return false;
+    this.senha = senha;
   }
 }
 
