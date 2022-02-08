45294632dc1249b14141824390b9139ab3cd9dbb
Adriel <adriel.fariasan@gmail.com>
2022-02-08 00:15:09 -0300

    Adiciona footer dinamico

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import pagLogin from "./paginas/Login";
 
 const conteudo = document.getElementById("content");
 conteudo.appendChild(pagLogin([]));
+
+const footer = document.querySelector("footer p");
+footer.innerText = `Adriel Faria, ${new Date().getFullYear()}`;
